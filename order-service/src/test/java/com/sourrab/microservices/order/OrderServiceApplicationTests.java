c658d7b9b0eec10c3104b01eb9bd87d424b514bf
Saurab Ghimire <sourrab@gmail.com>
2024-12-30 21:06:49 -0600

    api gateway for aggregating services

--- order-service/src/test/java/com/sourrab/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/sourrab/microservices/order/OrderServiceApplicationTests.java
@@ -1,4 +1,5 @@
 package com.sourrab.microservices.order;
+import com.sourrab.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,9 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -35,8 +40,9 @@ class OrderServiceApplicationTests {
                      "quantity": 1
                 }
                 """;
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
 
-		RestAssured.given()
+		var responseBodyString = RestAssured.given()
 				.contentType("application/json")
 				.body(submitOrderJson)
 				.when()
@@ -44,10 +50,8 @@ class OrderServiceApplicationTests {
 				.then()
 				.log().all()
 				.statusCode(201)
-				.body("id", Matchers.notNullValue())
-				.body("orderNumber", Matchers.notNullValue())
-				.body("skuCode", Matchers.equalTo("iphone_15"))
-				.body("price", Matchers.equalTo(1000))
-				.body("quantity", Matchers.equalTo(1));
+				.extract()
+				.body().asString();
+		assertThat(responseBodyString, Matchers.is("Order placed successfully"));
 	}
 }
